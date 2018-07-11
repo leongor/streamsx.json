ac5147d5f7f1b638d6dfdcf05c80716560a27e1a
leongor <leongor@gmail.com>
2018-07-11 11:07:35 +0300

    Added JsonFormatOptions tuple type and setFormatOptions function to customize options for JSON formatting. prefixToIgnore moved to the new JsonFormatOptions. New options introduced: keysToReplace, maxFractionSize, stripRoundFloats, tsFormat, tsUTC. Decimals now serialized instead of casting to floats (no precision lost).

--- com.ibm.streamsx.json/impl/include/JsonWriter.h
+++ com.ibm.streamsx.json/impl/include/JsonWriter.h
@@ -10,17 +10,45 @@
 
 // Define SPL types and functions.
 #include "SPL/Runtime/Function/SPLCast.h"
+#include "SPL/Runtime/Function/MathFunctions.h"
 #include "SPL/Runtime/Function/TimeFunctions.h"
 #include <SPL/Runtime/Type/Tuple.h>
 
+#include <iostream>
 #include "rapidjson/writer.h"
 #include "rapidjson/stringbuffer.h"
-#include <streams_boost/algorithm/string.hpp>
+#include <streams_boost/algorithm/string/predicate.hpp>
+#include <streams_boost/date_time/c_local_time_adjustor.hpp>
+#include <streams_boost/date_time/posix_time/posix_time.hpp>
 
+using namespace streams_boost::posix_time;
 using namespace rapidjson;
 using namespace SPL;
 
 namespace com { namespace ibm { namespace streamsx { namespace json {
+
+	typedef streams_boost::date_time::c_local_adjustor<ptime> cLocalAdjustor;
+	typedef SPL::map<SPL::rstring,SPL::rstring> KeysMap;
+	const int DefaultMaxFractionSize = 6; // apply max decimal places as std::fixed default
+
+	struct FormatOptions {
+
+		FormatOptions() :
+			keysToReplace(), prefixToIgnore(""), maxFractionSize(DefaultMaxFractionSize), stripRoundFloats(false), tsFormat(""), tsUTC(false) {}
+
+		template<typename Tuple>
+		FormatOptions(Tuple const& tuple) :
+			keysToReplace(tuple.get_keysToReplace()), prefixToIgnore(tuple.get_prefixToIgnore()),
+			maxFractionSize(tuple.get_maxFractionSize() ? tuple.get_maxFractionSize() : DefaultMaxFractionSize),
+			stripRoundFloats(tuple.get_stripRoundFloats()),	tsFormat(tuple.get_tsFormat()), tsUTC(tuple.get_tsUTC()) {}
+
+		KeysMap keysToReplace;
+		SPL::rstring prefixToIgnore;
+		int maxFractionSize;
+		bool stripRoundFloats;
+		SPL::rstring tsFormat;
+		bool tsUTC;
+	};
 
 	template<typename String>
 	inline char const* convToChars(String const& str) { return str.c_str(); }
@@ -46,54 +74,80 @@ namespace com { namespace ibm { namespace streamsx { namespace json {
 		}
 	}
 
-	template<typename Container, typename Iterator>
-	inline void writeArray(Writer<StringBuffer> & writer, ConstValueHandle const & valueHandle, SPL::rstring const& prefixToIgnore);
+	template<typename Floating>
+	inline bool checkStripFloat(Floating value, FormatOptions const& options) {
+		if(options.stripRoundFloats) {
+			const Floating presicion = pow(10, -options.maxFractionSize);
+			return fabs(value) < (int)fabs(value) + presicion;
+		}
+		return false;
+	}
+
+	inline size_t getDecimalSize(rstring const& valueStr) {
+		size_t dotPos = valueStr.find_first_of(".");
+		return (dotPos != std::string::npos) ? dotPos : valueStr.size();
+	}
 
 	template<typename Container, typename Iterator>
-	inline void writeMap(Writer<StringBuffer> & writer, ConstValueHandle const & valueHandle, SPL::rstring const& prefixToIgnore);
+	inline void writeArray(Writer<StringBuffer> & writer, ConstValueHandle const& valueHandle, FormatOptions const& options);
 
-	inline void writeTuple(Writer<StringBuffer> & writer, ConstValueHandle const & valueHandle, SPL::rstring const& prefixToIgnore);
+	template<typename Container, typename Iterator>
+	inline void writeMap(Writer<StringBuffer> & writer, ConstValueHandle const& valueHandle, FormatOptions const& options);
 
-	inline void writePrimitive(Writer<StringBuffer> & writer, ConstValueHandle const & valueHandle);
+	inline void writeTuple(Writer<StringBuffer> & writer, ConstValueHandle const& valueHandle, FormatOptions const& options);
 
+	inline void writePrimitive(Writer<StringBuffer> & writer, ConstValueHandle const& valueHandle, FormatOptions const& options);
 
-	inline void writeAny(Writer<StringBuffer> & writer, ConstValueHandle const & valueHandle, SPL::rstring const& prefixToIgnore) {
+	inline void writeKey(Writer<StringBuffer> & writer, std::string const& attrName, FormatOptions const& options) {
+
+		typename KeysMap::const_iterator iter = options.keysToReplace.find(attrName);
+		if(iter != options.keysToReplace.end())	// search key to replace in the options map
+			writer.Key(convToChars( iter->second));
+
+		else if(!options.prefixToIgnore.empty() && starts_with(attrName, options.prefixToIgnore)) // check if key is prefixed
+			writer.Key(convToChars(replace_first_copy(attrName, options.prefixToIgnore, "")));
+
+		else
+			writer.Key(convToChars(attrName));
+	}
+
+	inline void writeAny(Writer<StringBuffer> & writer, ConstValueHandle const& valueHandle, FormatOptions const& options) {
 
 		switch (valueHandle.getMetaType()) {
 			case Meta::Type::LIST : {
-				writeArray<List,ConstListIterator>(writer, valueHandle, prefixToIgnore);
+				writeArray<List,ConstListIterator>(writer, valueHandle, options);
 				break;
 			}
 			case Meta::Type::BLIST : {
-				writeArray<BList,ConstListIterator>(writer, valueHandle, prefixToIgnore);
+				writeArray<BList,ConstListIterator>(writer, valueHandle, options);
 				break;
 			}
 			case Meta::Type::SET : {
-				writeArray<Set,ConstSetIterator>(writer, valueHandle, prefixToIgnore);
+				writeArray<Set,ConstSetIterator>(writer, valueHandle, options);
 				break;
 			}
 			case Meta::Type::BSET : {
-				writeArray<BSet,ConstSetIterator>(writer, valueHandle, prefixToIgnore);
+				writeArray<BSet,ConstSetIterator>(writer, valueHandle, options);
 				break;
 			}
 			case Meta::Type::MAP :
-				writeMap<Map,ConstMapIterator>(writer, valueHandle, prefixToIgnore);
+				writeMap<Map,ConstMapIterator>(writer, valueHandle, options);
 				break;
 			case Meta::Type::BMAP : {
-				writeMap<BMap,ConstMapIterator>(writer, valueHandle, prefixToIgnore);
+				writeMap<BMap,ConstMapIterator>(writer, valueHandle, options);
 				break;
 			}
 			case Meta::Type::TUPLE : {
-				writeTuple(writer, valueHandle, prefixToIgnore);
+				writeTuple(writer, valueHandle, options);
 				break;
 			}
 			default:
-				writePrimitive(writer, valueHandle);
+				writePrimitive(writer, valueHandle, options);
 		}
 	}
 
 	template<typename Container, typename Iterator>
-	inline void writeArray(Writer<StringBuffer> & writer, ConstValueHandle const & valueHandle, SPL::rstring const& prefixToIgnore) {
+	inline void writeArray(Writer<StringBuffer> & writer, ConstValueHandle const& valueHandle, FormatOptions const& options) {
 
 		writer.StartArray();
 
@@ -101,14 +155,14 @@ namespace com { namespace ibm { namespace streamsx { namespace json {
 		for(Iterator arrayIter = array.getBeginIterator(); arrayIter != array.getEndIterator(); arrayIter++) {
 
 			const ConstValueHandle & arrayValueHandle = *arrayIter;
-			writeAny(writer, arrayValueHandle, prefixToIgnore);
+			writeAny(writer, arrayValueHandle, options);
 		}
 
 		writer.EndArray();
 	}
 
 	template<typename Container, typename Iterator>
-	inline void writeMap(Writer<StringBuffer> & writer, ConstValueHandle const & valueHandle, SPL::rstring const& prefixToIgnore) {
+	inline void writeMap(Writer<StringBuffer> & writer, ConstValueHandle const& valueHandle, FormatOptions const& options) {
 
 		writer.StartObject();
 
@@ -118,14 +172,14 @@ namespace com { namespace ibm { namespace streamsx { namespace json {
 			const std::pair<ConstValueHandle,ConstValueHandle> & mapHandle = *mapIter;
 			const ConstValueHandle & mapValueHandle = mapHandle.second;
 
-			writer.String(convToChars(mapHandle.first));
-			writeAny(writer, mapValueHandle, prefixToIgnore);
+			writer.Key(convToChars(mapHandle.first));
+			writeAny(writer, mapValueHandle, options);
 		}
 
 		writer.EndObject();
 	}
 
-	inline void writeTuple(Writer<StringBuffer> & writer, ConstValueHandle const & valueHandle, SPL::rstring const& prefixToIgnore) {
+	inline void writeTuple(Writer<StringBuffer> & writer, ConstValueHandle const& valueHandle, FormatOptions const& options) {
 		using namespace streams_boost::algorithm;
 
 		writer.StartObject();
@@ -136,20 +190,14 @@ namespace com { namespace ibm { namespace streamsx { namespace json {
 			const std::string & attrName = (*tupleIter).getName();
 			const ConstValueHandle & attrValueHandle = static_cast<ConstTupleAttribute>(*tupleIter).getValue();
 
-			if(!prefixToIgnore.empty() && starts_with(attrName, prefixToIgnore)) {
-				writer.String(convToChars(replace_first_copy(attrName, prefixToIgnore, "")));
-			}
-			else {
-				writer.String(convToChars(attrName));
-			}
-
-			writeAny(writer, attrValueHandle, prefixToIgnore);
+			writeKey(writer, attrName, options);
+			writeAny(writer, attrValueHandle, options);
 		}
 
 		writer.EndObject();
 	}
 
-	inline void writePrimitive(Writer<StringBuffer> & writer, ConstValueHandle const & valueHandle) {
+	inline void writePrimitive(Writer<StringBuffer> & writer, ConstValueHandle const& valueHandle, FormatOptions const& options) {
 
 		switch (valueHandle.getMetaType()) {
 			case Meta::Type::BOOLEAN : {
@@ -204,27 +252,39 @@ namespace com { namespace ibm { namespace streamsx { namespace json {
 			}
 			case Meta::Type::FLOAT32 : {
 				const float32 & value = valueHandle;
-				writer.Double(value);
+				if(checkStripFloat(value, options))
+					writer.Int(value);
+				else
+					writer.Double(value);
 				break;
 			}
 			case Meta::Type::FLOAT64 : {
 				const float64 & value = valueHandle;
-				writer.Double(value);
+				if(checkStripFloat(value, options))
+					writer.Int64(value);
+				else
+					writer.Double(value);
 				break;
 			}
 			case Meta::Type::DECIMAL32 : {
-				const float64 & value = spl_cast<float64,decimal32>::cast(valueHandle);
-				writer.Double(value);
+				const decimal32 & value = valueHandle;
+				const rstring & valueStr = spl_cast<rstring,decimal32>::cast(value);
+				size_t valueSize = (options.stripRoundFloats && value == Functions::Math::floor(value)) ? getDecimalSize(valueStr) : valueStr.size();
+				writer.RawValue(convToChars(valueStr), valueSize, kNumberType);
 				break;
 			}
 			case Meta::Type::DECIMAL64 : {
-				const float64 & value = spl_cast<float64,decimal64>::cast(valueHandle);
-				writer.Double(value);
+				const decimal64 & value = valueHandle;
+				const rstring & valueStr = spl_cast<rstring,decimal64>::cast(value);
+				size_t valueSize = (options.stripRoundFloats && value == Functions::Math::floor(value)) ? getDecimalSize(valueStr) : valueStr.size();
+				writer.RawValue(convToChars(valueStr), valueSize, kNumberType);
 				break;
 			}
 			case Meta::Type::DECIMAL128 : {
-				const float64 & value = spl_cast<float64,decimal128>::cast(valueHandle);
-				writer.Double(value);
+				const decimal128 & value = valueHandle;
+				const rstring & valueStr = spl_cast<rstring,decimal128>::cast(value);
+				size_t valueSize = (options.stripRoundFloats && value == Functions::Math::floor(value)) ? getDecimalSize(valueStr) : valueStr.size();
+				writer.RawValue(convToChars(valueStr), valueSize, kNumberType);
 				break;
 			}
 			case Meta::Type::COMPLEX32 : {
@@ -237,7 +297,21 @@ namespace com { namespace ibm { namespace streamsx { namespace json {
 			}
 			case Meta::Type::TIMESTAMP : {
 				const timestamp & value = valueHandle;
-				writer.String(convToChars(Functions::Time::ctime(value)));
+
+				if(options.tsFormat.empty()) {
+					writer.String(convToChars(Functions::Time::ctime(value)));
+				}
+				else {
+					std::stringstream stream;
+					stream.imbue(std::locale(std::locale::classic(), new time_facet(options.tsFormat.c_str()))); // time_facet pointer is managed by std::locale::facet
+					const ptime dateTime( ptime( from_time_t(value.getSeconds())) + microseconds(value.getNanoseconds() / 1000));
+					if(options.tsUTC)
+						stream << dateTime << 'Z';
+					else
+						stream << cLocalAdjustor::utc_to_local(dateTime);
+
+					writer.String(convToChars(stream.str()));
+				}
 				break;
 			}
 			case Meta::Type::BSTRING :
@@ -260,39 +334,31 @@ namespace com { namespace ibm { namespace streamsx { namespace json {
 	}
 
 
-	inline SPL::rstring tupleToJSON(Tuple const& tuple, SPL::rstring prefixToIgnore = "") {
+	template<typename Composite>
+	inline SPL::rstring toJSONImpl(Composite const& comp, FormatOptions const& options) {
 
 	    StringBuffer s;
 	    Writer<StringBuffer> writer(s);
+	    writer.SetMaxDecimalPlaces(options.maxFractionSize);
 
-		writeAny(writer, ConstValueHandle(tuple), prefixToIgnore);
+		writeAny(writer, ConstValueHandle(comp), options);
 
 		return s.GetString();
 	}
 
-	template<class MAP>
-	inline SPL::rstring mapToJSON(MAP const& map, SPL::rstring prefixToIgnore = "") {
-
-	    StringBuffer s;
-	    Writer<StringBuffer> writer(s);
-
-		writeAny(writer, ConstValueHandle(map), prefixToIgnore);
-
-		return s.GetString();
-	}
-
-	template<class String, class SPLAny>
-	inline SPL::rstring toJSON(String const& key, SPLAny const& splAny, SPL::rstring prefixToIgnore = "") {
+	template<typename String, typename SPLAny>
+	inline SPL::rstring toJSONImpl(String const& key, SPLAny const& splAny, FormatOptions const& options) {
 
 
 	    StringBuffer s;
 	    Writer<StringBuffer> writer(s);
+	    writer.SetMaxDecimalPlaces(options.maxFractionSize);
 
 		writer.StartObject();
 
-		writer.String(convToChars(key));
+		writer.Key(convToChars(key));
 
-		writeAny(writer, ConstValueHandle(splAny), prefixToIgnore);
+		writeAny(writer, ConstValueHandle(splAny), options);
 
 		writer.EndObject();
 
@@ -302,3 +368,49 @@ namespace com { namespace ibm { namespace streamsx { namespace json {
 }}}}
 
 #endif /* JSON_WRITER_H_ */
+
+namespace com { namespace ibm { namespace streamsx { namespace json {
+
+	namespace { // this anonymous namespace will be defined for each operator separately
+
+		inline FormatOptions const& getFormatOptions(FormatOptions const* fo = NULL) {
+
+			static const FormatOptions _fo(fo ? *fo : FormatOptions());
+			return _fo;
+		}
+
+		template<typename Tuple>
+		inline bool setFormatOptions(Tuple const& options) {
+
+			const FormatOptions fo(options);
+			getFormatOptions(&fo);
+			return true;
+		}
+
+
+		inline SPL::rstring tupleToJSON(Tuple const& tuple) {
+
+			return toJSONImpl(tuple, getFormatOptions());
+		}
+
+		template<typename MAP>
+		inline SPL::rstring mapToJSON(MAP const& map) {
+
+			return toJSONImpl(map, getFormatOptions());
+		}
+
+		template<typename String, typename SPLAny>
+		inline SPL::rstring toJSON(String const& key, SPLAny const& splAny) {
+
+			return toJSONImpl(key, splAny, getFormatOptions());
+		}
+
+		template<typename String, typename SPLAny, typename KeysMap>
+		inline SPL::rstring toJSON(String const& key, SPLAny const& splAny) {
+
+			return toJSONImpl(key, splAny, getFormatOptions());
+		}
+
+	}
+
+}}}}
